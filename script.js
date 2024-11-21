111e2705a48d8b304ae9a5477da7c576d23df379
DanteWHS <dante130283@student-schoolsofwestfield.org>
2024-11-21 17:12:10 +0000

    notdone

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function setRound() {
+    let set = prompt("How many rounds?");
+    if (set % 2 == 0 || isNaN(set) == true) {
+       alert("Must be an odd number");
+       return setRound();
+    } else{
+        alert("Best out of "+set)
+    return set;}
+ }
