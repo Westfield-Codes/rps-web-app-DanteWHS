54a8133195f30a1484bc7b45fd5b2cd282168f66
DanteWHS <dante130283@student-schoolsofwestfield.org>
2024-11-25 17:12:22 +0000

    not done

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+var round = 1;
+var set = 0;
+localStorage.setItem("rounds", set);
 function getRounds() {
-    let set = document.getElementById("rounds").value;
+   set = document.getElementById("rounds").value;
     setRound(set);
 }
 
 function setRound(set) {
-    let rounds = 1
     if (set % 2 == 0 || isNaN(set)==true) {
        alert("Must be an odd number");
     } else{
-        alert("Best out of "+ set);
-    localStorage.setItem("set", set)
-    
+    localStorage.setItem("rounds", set)
     window.location.replace("chooser.html");}
  }
 
+ function showRound(){
+   let set = localStorage.getItem("rounds");
+   let statsBox = document.getElementById("statsBox");
+   let message = "Round " + round + " out of " + set;
+   statsBox.innerHTML = message;
+ }
+
  function cTurn(u){
-    let moves = ["r", "p", "s"];
-    let choice = Math.floor(Math.random()*3);
-    let c = moves[choice];
-    findWinner(u, c)
+   let moves = ["r", "p", "s"];
+   let choice = Math.floor(Math.random()*3);
+   let c = moves[choice];
+   if (c==u){
+      alert("We both chose " + u);
+   }
+   else findWinner(u, c);
  }
  
- function findWinner(u, c, rounds){
+ function findWinner(u, c){
+   round++
+   console.log(round);
     let winArray = [
        ["r", "p", "Computer"], 
        ["p", "s", "Computer"], 
@@ -31,8 +43,9 @@ function setRound(set) {
        ["p", "r", "You"]
     ];
     for (var i = 0; i<winArray.length; i++){
-        if (winArray[i][0]+winArray[i][1]==uChoice+cChoice) break;
+        if (winArray[i][0]+winArray[i][1]==u+c) break;
+        
     }
-    alert(uChoice + " Vs. " + cChoice + ". " + winArray[i][2] + " won!");
-    rounds+=1
+    alert(u + " Vs. " + c + ". " + winArray[i][2] + " won!");
+    showRound();
  }
